4e0efd2a518acac9d56a255c156654dcc1b3ddd1
rina <pnd1117@gmail.com>
2020-01-31 20:19:51 +0900

    Z

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import PropTypes from 'prop-types'
 
-class App extends Component {
-  render() {
-    return<div><h1>Hello World!</h1></div>;
+
+const App=()=>{
+  const profiles=[
+    {name:'Taro',age:10},
+    {name:'Hanako',age:5},
+    {name:'Noname',age:3}
+  ]
+  return (
+  <div>
+  {
+    profiles.map((profile,index)=>{
+      return <User name={profile.name} age={profile.age} key={index}/>
+    })
   }
+  </div>
+  );
+}
+
+const User=(props)=>{
+return <div>Hi,I'm {props.name},and {props.age} years old!</div>
+}
+
+User.propTypes={
+  name:PropTypes.string,
+  age:PropTypes.number.isRequired
 }
 
 export default App;
